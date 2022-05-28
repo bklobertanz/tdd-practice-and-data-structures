81b4ec73a281b363911f4eab2743e755f7ea1ac9
Barís Klobertanz <klobertanz@gmail.com>
2022-05-27 22:50:30 -0400

    feat(Queue): add application example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import 'dotenv/config'
-
+import Queue from './data-structures/queue'
 import reverseString from './utils/reverseString'
 
+//applying stack
 const word = 'pizza with pineapple'
 const invertedWord = reverseString(word)
 console.log('word -> ', word)
 console.log('reversedWord -> ', invertedWord)
+
+//applying queue
+const printerQueue = new Queue()
+printerQueue.enqueue({ jobName: 'test', pages: '10' })
+printerQueue.enqueue({ jobName: 'test 2', pages: '1' })
+printerQueue.enqueue({ jobName: 'test 3', pages: '5' })
+console.log(printerQueue.peek())
+while (printerQueue.head <= printerQueue.tail) {
+  console.log('Printed job: ', printerQueue.dequeue())
+}
+console.log(printerQueue.dequeue())
